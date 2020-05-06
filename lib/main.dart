7e6a1377c7604829ba468101e1841a55f32a0b22
OM KUMAR <om.kumar@myglamm.com>
2020-05-06 05:47:54 +0530

    add and delete note feature added'

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/screens/noteList.dart';
+import 'package:note_app/screens/noteDetail.dart';
 
 
-void main() => runApp(NoteList());
+void main() => runApp(MaterialApp(
+  title: "StateFull Example",
+  home: NoteApp(),
+));
 
-class NoteList extends StatelessWidget {
+
+//void main() {
+//  print("Programm Starts");
+//  printFileContent();
+//  print("Programme Ends");
+//}
+
+
+class NoteApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      title: "Home Data",
-      home: HomeScreen(),
+      title: "Note Keeper",
+      home: MyNoteList() ,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
     );
   }
-
 }
-class HomeScreen extends StatelessWidget {
 
-  String title = "press me!";
+// HOW async and a
+ printFileContent() async {
+  String file = await downloadFile();
+  print("The file is -> $file");
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Return Data"),
-      ),
-      body: Center(
-        child: SelectionButton(),
-      ),
-    );
-  }
+Future<String> downloadFile() {
 
-  }
+   Future<String> result = Future.delayed(Duration(seconds: 6), () {
 
-
-  class SelectionButton extends StatelessWidget {
-
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return RaisedButton(
-      onPressed: () {
-        navigateToSelectionScreen(context) ;},
-      child: (
-      Text(
-        // I wamt to set this button title on pop back the whatever the option choose.
-        //"$HomeScreen().title" 
-          "Press me!"
-      )
-      ),
-    );
-  }
-  }
-
-    navigateToSelectionScreen(BuildContext context) async {
-    final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => SelectionScreen() ));
-    
-  HomeScreen().title = result;
-  Scaffold.of(context)
-    ..removeCurrentSnackBar()
-    ..showSnackBar(SnackBar(content: Text("$result"),));
-
-  }
-
-class SelectionScreen extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Pick An Option"
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              onPressed: () { Navigator.pop(context, "Yes It is");},
-              child: Text(
-                "Press Yes!"
-              ),
-            ),
-            RaisedButton(
-              onPressed: () { Navigator.pop(context, "No its Not"); },
-              child: Text(
-                "Press No!"
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+     return "The Download File";
+   });
+return result;
 }
